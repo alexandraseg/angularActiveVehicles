8eab99ab55b9a49b7a9e5c8c877bf933c1511425
Alexandra <alex.segkou@gmail.com>
2024-04-24 13:23:29 +0300

    initial commit of the source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,43 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  isExpanded = false;
+  showSubmenu = false;
+  isShowing = false;
+  showSubSubMenu = false;
+
   title = 'AngularProjectVehicles';
+  displayedColumns: string[] = ['serialNumber', 'fullName', 'issueDate', 'isApproved', 'tierAmount', 'registrationAmount', 'consumptionAmount', 'rewardAmount'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private http: HttpClient) {} // Inject HttpClient
+
+  ngOnInit() {
+    this.http.get<any[]>('assets/records.json').subscribe(records => {
+      this.dataSource = new MatTableDataSource(records);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  parseDate(dateString: string): Date {
+    // Assuming dateString is in the format YYYYMMDD
+    const year = parseInt(dateString.substring(0, 4), 10);
+    const month = parseInt(dateString.substring(4, 6), 10) - 1; // Months are zero-based
+    const day = parseInt(dateString.substring(6, 8), 10);
+    return new Date(year, month, day);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,60 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { RouterOutlet } from '@angular/router';
+import {MatSortModule} from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+// import { MenubarComponent } from './component/menubar/menubar.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbar } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // MenubarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    RouterOutlet,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginator,
+    MatSort,
+    MatSortModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule,
+    MatToolbar,
+    
+    // MatTableDataSource
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
