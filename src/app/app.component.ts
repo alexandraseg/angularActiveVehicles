4edd13c94884ea3258a8e679247a39a1dd1ada7d
Alexandra <alex.segkou@gmail.com>
2024-05-01 15:29:30 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,19 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('input') input: any;
+
+  vehicles: any[];
+  selectedVehiclePlate: string | null = null;
+  records: any[];
+  drivers: any[];
+
+  serialNumber: string | null = null;
+  selectedDriver: string | null = null;
+  startDate: Date | null = null;
+  endDate: Date | null = null;
+  selectedStatus: string = 'all'; // Default to 'all'
 
   isExpanded = false;
   showSubmenu = false;
@@ -24,14 +36,38 @@ export class AppComponent {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private http: HttpClient) {} // Inject HttpClient
+  constructor(private http: HttpClient) {
+    this.vehicles = [];
+    // Fetch data from the vehicles.json file
+    this.http.get<any[]>('assets/vehicles.json').subscribe(data => {
+      this.vehicles = data;
+    });
 
-  ngOnInit() {
+    this.records = [];
+    // Fetch data from the records.json file
     this.http.get<any[]>('assets/records.json').subscribe(records => {
+      this.records = records;
       this.dataSource = new MatTableDataSource(records);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     });
+
+    this.drivers = [];
+    // Fetch data from the drivers.json file
+    this.http.get<any[]>('assets/drivers.json').subscribe(drivers => {
+      this.drivers = drivers;
+    });
+  } 
+
+  ngOnInit() {}
+
+  onDriverSelectionChange(event: any) {
+    this.selectedDriver = event.value;
+  }
+
+  
+  onStatusSelectionChange(event: any) {
+    this.selectedStatus = event.value;
   }
 
   applyFilter(event: Event) {
@@ -43,11 +79,83 @@ export class AppComponent {
     }
   }
 
+  applyFilterDriver(driverName: string) {
+    this.dataSource.filter = driverName.trim().toLowerCase();
+  
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+  applyFilterStatus(status: string) {
+    this.dataSource.filter = status.trim().toLowerCase();
+  
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  search() {
+    // console.log('Search method called');
+    // Get selected filter values
+    const serialNumber = this.serialNumber;
+    const selectedDriver = this.selectedDriver;
+    const startDate = this.startDate;
+    const endDate = this.endDate;
+    const status = this.selectedStatus;
+    // console.log('Filters:', serialNumber, selectedDriver, status, startDate, endDate);
+
+    // Filter records based on selected criteria
+    let filteredRecords = this.records;
+
+    if (serialNumber) {
+      filteredRecords = filteredRecords.filter(record => record.serialNumber === serialNumber);
+    }
+  
+    if (selectedDriver) {
+      filteredRecords = filteredRecords.filter(record => record.fullName === selectedDriver);
+    }
+
+    if (startDate && endDate) {
+      filteredRecords = filteredRecords.filter(record => {
+        const issueDate = this.parseDate(record.issueDate);
+        // console.log('issueDate:', issueDate)
+        // console.log('startDate:', startDate)
+        // console.log('endDate:', endDate)
+        return issueDate >= startDate && issueDate <= endDate;
+      });
+    }
+  
+    if (status !== 'all') {
+      const statusValue = status === 'true'; 
+      filteredRecords = filteredRecords.filter(record => record.isApproved === statusValue);
+    }
+  
+    // Update data source with filtered records
+    this.dataSource.data = filteredRecords;
+  }
+
   parseDate(dateString: string): Date {
-    // Assuming dateString is in the format YYYYMMDD
+    // format YYYYMMDD
     const year = parseInt(dateString.substring(0, 4), 10);
     const month = parseInt(dateString.substring(4, 6), 10) - 1; // Months are zero-based
     const day = parseInt(dateString.substring(6, 8), 10);
     return new Date(year, month, day);
+  }
+
+  selectVehicle(plate: string) {
+    this.selectedVehiclePlate = plate;
+
+    const filteredRecords = this.records.filter(record => record.plate === plate);
+
+    filteredRecords.forEach(record => {
+      const driver = this.drivers.find(driver => driver.id === record.driverId);
+      if (driver) {
+        record.fullName = driver.fullName;
+      }
+    });
+
+    // // Assign the filtered records to the data source
+    this.dataSource.data = filteredRecords;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,32 @@
 import { NgModule, ViewChild } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
 import { RouterOutlet } from '@angular/router';
 import {MatSortModule} from '@angular/material/sort';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { MatInputModule } from '@angular/material/input';
-
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
-// import { MenubarComponent } from './component/menubar/menubar.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbar } from '@angular/material/toolbar';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // MenubarComponent
   ],
   imports: [
     BrowserModule,
@@ -49,9 +46,12 @@ import { MatToolbar } from '@angular/material/toolbar';
     MatListModule,
     MatMenuModule,
     MatToolbar,
-    
-    // MatTableDataSource
-  ],
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+      ],
   providers: [
     provideAnimationsAsync()
   ],
